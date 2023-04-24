24550fb91b7cd958a18008119662ba4eab7a33e9
lvrd-ade <121715011+lvrd-ade@users.noreply.github.com>
2023-04-23 20:21:23 -0500

    Add files via upload

---
+++ FinalProjectTemplate.c
@@ -0,0 +1,38 @@
+//SE 185: Final Project Template//
+/////////////////////////
+/* 
+Team member 1 "Name" | "Percentage of Contribution to The Project"
+Team member 2 "Name" | "Percentage of Contribution to The Project"
+Team member 3 "Name" | "Percentage of Contribution to The Project"
+Team member 4 "Name" | "Percentage of Contribution to The Project"
+*/
+
+////////////////////
+#include<stdio.h>//
+//add more here////
+
+//////////////////////
+//Struct Definition//
+////////////////////
+
+
+/////////////////////////////////////
+//User Defined Functions Prototype//
+//List prototypes here and define//
+//tehm below the main function////
+/////////////////////////////////
+
+int main(){
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+///////////////////////////////////////
+//User Defined Functions' Definition//
+/////////////////////////////////////
